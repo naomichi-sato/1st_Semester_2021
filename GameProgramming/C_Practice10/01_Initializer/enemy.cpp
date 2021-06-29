b4fa618dc9ae40e595f6a8b1dd3e93e2ce3546df
n_sato <n.satou@monochrome-corp.com>
2021-06-29 16:53:54 +0900

    手順５～８まで完了
    enemy.h、enemy.cppの追加と内容の実装

---
+++ GameProgramming/C_Practice10/01_Initializer/enemy.cpp
@@ -0,0 +1,40 @@
+﻿
+#include "enemy.h"
+
+// コンストラクタ
+Enemy::Enemy()
+	: Hp(8)
+	, Mp(1)
+	, Atk(2)
+	, Def(1)
+{
+	printf( "コンストラクタ\n" );
+	Name[0] = '\0';
+}
+
+// デストラクタ
+Enemy::~Enemy()
+{
+	printf( "デストラクタ\n" );
+}
+
+// ステータス初期化
+void Enemy::InitStatus(const char* enemyName)
+{
+	sprintf_s( Name, 32, "%s", enemyName );
+	Hp = 8;
+	Atk = 2;
+	Def = 1;
+}
+
+// ステータス表示
+void Enemy::ShowStatus()
+{
+	printf("--- Enemy Status ---\n");
+	printf("Name : %s\n", Name);
+	printf("Hp   : %02d\n", Hp );
+	printf("Mp   : %02d\n", Mp );
+	printf("Atk  : %02d\n", Atk );
+	printf("Def  : %02d\n", Def );
+}
+
---
+++ GameProgramming/C_Practice10/01_Initializer/enemy.h
@@ -0,0 +1,29 @@
+﻿
+#ifndef ENEMY_H
+#define ENEMY_H
+
+#include <stdio.h>
+
+class Enemy
+{
+public:
+	// 第９回
+	Enemy();	// コンストラクタ
+	~Enemy();	// デストラクタ
+
+	void InitStatus(const char* enemyName);		// ステータス初期化
+	void ShowStatus();							// ステータス表示
+
+	// メンバ変数
+	char Name[32];
+	int Hp;
+
+	int Mp;
+
+private:
+	int Atk;
+	int Def;
+};
+
+#endif
+
