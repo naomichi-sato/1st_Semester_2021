0e14f1e72e83614cdd206659ac047d1b9e43f2ba
n_sato <n.satou@monochrome-corp.com>
2021-07-06 17:43:34 +0900

    第８回_作業01_完了

---
+++ GameProgramming/C_Practice08/01_Class/main.cpp
@@ -0,0 +1,35 @@
+﻿
+#include <stdio.h>
+#include <stdlib.h>
+
+struct StructHoge
+{
+	int value;
+};
+
+class ClassHoge
+{
+	int value;
+	void PrintHelloWorld();
+};
+
+void ClassHoge::PrintHelloWorld()
+{
+	printf("HelloWorld\n");
+}
+
+int main( void )
+{
+	StructHoge struct_hoge;
+	ClassHoge class_hoge;
+
+	struct_hoge.value = 1;
+	class_hoge.value = 10;
+	class_hoge.PrintHelloWorld();
+
+	system("pause");
+	return 0;
+}
+
+
+
